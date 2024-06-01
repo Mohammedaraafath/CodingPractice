0741419cdad6f5c45096645c4af905b2c18abdab
Araafath <araafa786@gmail.com>
2024-06-01 20:26:31 +0530

    String Reverse

---
+++ StringReverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//String 12345 is given need to do sting reverse
+//need to get input from user
+public class StringReverse {
+    public static void main(String[] args) {
+        System.out.println("Enter the string you want to reverse it");
+        Scanner inputFromUser=new Scanner(System.in); //getting input we need to create the object
+        String UserInput=inputFromUser.next();
+        System.out.println(UserInput);
+
+
+    }
+
+}
